b53b3fc1cd0373a16c29ffbd6468ab09a83769f4
Ahmad Fernando <ahmadfernando88@gmail.com>
2024-12-29 23:25:45 +0700

    login

--- resources/views/guest/layouts/master.blade.php
+++ resources/views/guest/layouts/master.blade.php
@@ -28,7 +28,7 @@
                 <span class="self-center text-2xl text-white font-semibold whitespace-nowrap">SMK Kartika Medan</span>
             </a>
             <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <button type="button" class="text-dark bg-blue-300 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center">Login</button>
+                <a href="/login" type="button" class="text-dark bg-blue-300 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center">Login</a>
                 <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-cta" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
                     <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
