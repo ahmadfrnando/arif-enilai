9748e696d0f91c9cf4be16a65a4f0adb7f66e453
Ahmad Fernando <ahmadfernando88@gmail.com>
2025-04-28 18:12:51 +0700

    buat ulang pelaksanaan semester

---
+++ database/migrations/2025_04_28_111143_recreate_pelaksanaan_semester_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {   
+        Schema::dropIfExists('pelaksanaan_semester');
+
+        Schema::create('pelaksanaan_semester', function (Blueprint $table) {
+            $table->id();
+            $table->enum('semester', ['1', '2']);
+            $table->boolean('status_aktif')->default(0);
+            $table->timestamps();
+        });
+
+        DB::table('pelaksanaan_semester')->insert([
+            'id' => 1,
+            'semester' => '1',
+            'tahun_ajaran' => '2025/2026',
+            'status_aktif' => '0',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pelaksanaan_semester');
+    }
+};
